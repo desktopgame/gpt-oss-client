d842bff77dc266cf9d931e9bc96aed9b4d243b08
desktopgame <>
2025-08-08 20:32:07 +0900

    Update: OpenAIのテスト

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from gpt-oss-client!")
+import os
+import asyncio
+from openai import AsyncOpenAI
+
+client = AsyncOpenAI(api_key="lmstudio", base_url="http://localhost:1234/v1")
+
+
+async def main() -> None:
+    response = await client.responses.create(
+        model="openai/gpt-oss-120b", input="こんにちわ"
+    )
+    print(response.output_text)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
