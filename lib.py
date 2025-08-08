145f361e4c310e0f4056a7a9fc7ed49e4cccab13
desktopgame <>
2025-08-08 20:48:45 +0900

    Update: MCP起動処理

---
+++ lib.py
@@ -0,0 +1,54 @@
+import asyncio
+import os
+import json
+from typing import Any
+
+
+class Connection:
+    def __init__(self, config):
+        self.config = config
+        self.proc = None
+
+    async def setup(self):
+        env = os.environ
+        if "env" in self.config:
+            env = { **env, **self.config["env"] }
+
+        cwd = None
+        if "cwd" in self.config:
+            cwd = self.config["cwd"]
+
+        command = self.config["command"]
+        args = self.config["args"]
+        self.proc = await asyncio.subprocess.create_subprocess_shell(
+            ' '.join([command, *args]),
+            shell=True,
+            env=env,
+            cwd=cwd,
+            stdin=asyncio.subprocess.PIPE,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.DEVNULL
+        )
+
+    async def shutdown(self):
+        self.proc.stdin.close()
+        while not self.proc.stdout.at_eof():
+            await self._readline(1)
+        self.proc.kill()
+
+    async def _readline(self, timeout: float):
+        stream = self.proc.stdout
+        return await asyncio.wait_for(stream.readline(), timeout=timeout)
+
+    async def send(self, data: Any):
+        self.proc.stdin.write(json.dumps(data).encode())
+        self.proc.stdin.write("\n".encode())
+        await self.proc.stdin.drain()
+
+    async def receive(self) -> Any:
+        line = await self._readline()
+        return json.loads(line)
+
+    def is_exited(self):
+        status = self.proc.returncode
+        return status is not None
--- main.py
+++ main.py
@@ -1,15 +1,38 @@
-import os
 import asyncio
+import json
+import lib
 from openai import AsyncOpenAI
+from typing import Dict, Any
 
 client = AsyncOpenAI(api_key="lmstudio", base_url="http://localhost:1234/v1")
 
 
 async def main() -> None:
+    mcp_config: Dict[str, Any] = {}
+    try:
+        with open("mcp.json", "r", encoding="UTF-8") as fp:
+            mcp_config = json.load(fp)
+    except Exception:
+        pass
+
+    mcp_connections: Dict[str, lib.Connection] = {}
+    for name, server in mcp_config["mcpServers"].items():
+        conn = lib.Connection(server)
+        mcp_connections[name] = conn
+        await conn.setup()
+        await asyncio.sleep(1)
+        if conn.is_exited():
+            print(f"{name} is exited.")
+        else:
+            print(f"{name} is started.")
+
     response = await client.responses.create(
         model="openai/gpt-oss-120b", input="こんにちわ"
     )
     print(response.output_text)
+
+    for name, conn in mcp_connections.items():
+        await conn.shutdown()
 
 
 if __name__ == "__main__":
